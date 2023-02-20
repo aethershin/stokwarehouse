79ede98fa11f1f46a450b7439c152e753d65179e
aethershin <zikryafwan@gmail.com>
2023-02-20 18:21:22 +0700

    Update footer.php

--- application/views/backend/_partials/footer.php
+++ application/views/backend/_partials/footer.php
@@ -1,11 +1,11 @@
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
-                    <div class="float-start">
-                        <p>Copyright © <?php echo date('Y');?> <?php echo $site_title; ?>. All Rights Reserved </a></p>
-                    </div>
-                    <div class="float-end">
-                        <p>Developed with <span class='text-danger'><i class="bi bi-heart"></i></span> by <a
-                                href="https://www.instagram.com/aethershin_/" target="blank" title="ig">AetherShin</a></p>
-                    </div>
+		<div class="float-start">
+                            <p>Copyright  &copy; <?php echo date('Y'); ?> <strong><span><?php echo $site_title; ?></span></strong>. All Rights Reserved | Developed By <a rel="nofollow" href="https://www.instagram.com/aethershin_/" target="blank" title="ig">AetherShin</a></p>
+                        </div>
+                        <div class="float-end">
+                            <p>Template with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                                href="https://saugi.me">Saugi</a></p>
+                        </div>
                 </div>
             </footer>
