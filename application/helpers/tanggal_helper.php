fad6aa6a70cfa65b624c75f64fde8887aa980d3a
aethershin <zikryafwan@gmail.com>
2023-02-20 14:15:34 +0700

    Add files via upload

---
+++ application/helpers/tanggal_helper.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if (!function_exists('format_indo')) {
+  function format_indo($date){
+    date_default_timezone_set('Asia/Jakarta');
+    // array hari dan bulan
+    $Hari = array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
+    $Bulan = array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
+    
+    // pemisahan tahun, bulan, hari, dan waktu
+    $tahun = substr($date,0,4);
+    $bulan = substr($date,5,2);
+    $tgl = substr($date,8,2);
+    $waktu = substr($date,11,5);
+    $hari = date("w",strtotime($date));
+    $result = $Hari[$hari].", ".$tgl." ".$Bulan[(int)$bulan-1]." ".$tahun." ".$waktu;
+
+    return $result;
+  }
+}
